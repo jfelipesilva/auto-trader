97c83574ef1bd4fb62e64159b8597157c216e42a
João <jfelipesilva@gmail.com>
2018-09-16 21:34:43 -0300

    starting to build live trades in bitfinex

--- bitfinex_websocket.js
+++ bitfinex_websocket.js
@@ -1,4 +1,7 @@
 const crypto = require('crypto-js');
+//const crypto = require('crypto');
+const request = require('request');
+const moment = require('moment');
 const bf_ws = require('ws'); //bifinex websocket
 const lh_ws = require('ws'); //localhost websocket
 const env = require(__dirname + '/_configs');
@@ -85,24 +88,6 @@ wss.onmessage = (msg) => {
 
 };
 wss.onopen = () => {
-    // API keys setup here (See "Authenticated Channels")
-    const apiKey = env.BITFINEX_APIKEY;
-    const apiSecret = env.BITFINEX_APISECRET;
-
-    const authNonce = Date.now() * 1000;
-    const authPayload = 'AUTH' + authNonce;
-    const authSig = crypto
-        .HmacSHA384(authPayload, apiSecret)
-        .toString(crypto.enc.Hex)
-
-    const payload = {
-        apiKey,
-        authSig,
-        authNonce,
-        authPayload,
-        //dms: 4, uncomment to enable dead-man-switch
-        event: 'auth'
-    };
 
     for(var prop in pairs){
         utils.log("sending subscription request for "+prop);
@@ -113,6 +98,7 @@ wss.onopen = () => {
         }));
     }
 
+    //buy(1,1,'IOTUSD', '0.2');
 };
 
 priceChanges = (channel, lastPrice) => {
@@ -199,4 +185,150 @@ verifyStrategiesByPrice = (pair) => {
 
     });
 
+};
+
+buy = (user_id, exchange_id, pair, price) => {
+    /*
+    https://docs.bitfinex.com/v2/docs/abbreviations-glossary
+    te: trade executed;
+    tu: trade updated;
+    wu: wallet updates;
+    oc: order cancel;
+    os: order snapshot;
+
+    */
+
+    const bf_pair = "t"+pair;
+    let wss_auth = {};
+    let CHAN_ID = "";
+
+    database.query('SELECT * FROM user_has_exchange WHERE user_id = '+user_id+' AND exchange_id = '+exchange_id, function(rows) {
+        if(rows.length > 0){
+
+            wss_auth = new bf_ws('wss://api.bitfinex.com/ws/2');
+
+            wss_auth.onmessage = (msg) => {
+                //console.log(msg);
+                let messages = JSON.parse(msg.data);
+                if(messages.event == "auth" && messages.status == "OK" && messages.caps.orders.read==1 && messages.caps.orders.write==1){
+                    
+                    //LOGGED IN
+                    
+                    //SEARCH FOR OPEN ORDERS
+                    CHAN_ID = messages.chanId;
+                    getAllOrders();
+
+                    //wss_auth.close();
+                    console.log(messages);
+                }else if(messages.event == "auth" && messages.status == "FAILED"){
+                    utils.log("Falha de autenticação => "+messages.msg);
+                    wss_auth.close();
+                }else if(messages.event == "auth" && messages.status == "OK" && messages.caps.orders.read==1 && messages.caps.orders.write==0){
+                    utils.log("sem permissão de escrita");
+                    wss_auth.close();
+                }else if(Array.isArray(messages[2]) && messages[1] == "n" && messages[2][1] == "on-req" && messages[2][4][3] == bf_pair && messages[2][6] == "ERROR"){
+                    //UPDATES
+                    utils.log("DEU ERRO NA TRANSAÇÃO:"+ messages[2][7]);
+                    wss_auth.close();
+                }else if(Array.isArray(messages[2]) && messages[1] == "os"){
+
+                    //OPEN ORDERS
+                    console.log(messages);
+                    cancellAll(messages[2]);
+
+                }else if(Array.isArray(messages[2])){
+                    utils.log(messages);
+                    utils.log("ARRAY--");
+                    //echoNestedArray(messages[2]);
+                    utils.log("");
+                    utils.log("");
+                }else{
+                    console.log(messages);
+                }
+            };
+
+            wss_auth.onopen = () => {
+
+                const apiKey = rows[0].key;
+                const apiSecret = rows[0].secret;
+
+                const authNonce = Date.now() * 1000;
+                const authPayload = 'AUTH' + authNonce;
+                const authSig = crypto
+                    .HmacSHA384(authPayload, apiSecret)
+                    .toString(crypto.enc.Hex)
+
+                const payload = {
+                    apiKey,
+                    authSig,
+                    authNonce,
+                    authPayload,
+                    //dms: 4, uncomment to enable dead-man-switch
+                    event: 'auth'
+                };
+
+                wss_auth.send(JSON.stringify(payload));
+
+            };
+
+
+        }
+    });
+
+    cancellAll = (data) => {
+        if(data.length > 0){
+
+            let ids = [];
+            data.forEach(function(res, i){
+                ids.push(res[0]);
+            });
+            wss_auth.send(JSON.stringify([
+                0,
+                "oc_multi",
+                null,
+                {
+                    "id": ids
+                }
+            ]));
+
+            setTimeout(getAllOrders,2000);
+
+        }else{
+
+            //ORDER
+            wss_auth.send(JSON.stringify([
+                0,
+                'on',
+                null,
+                {
+                    cid: Date.now(),
+                    type: "EXCHANGE LIMIT",
+                    symbol: bf_pair,
+                    amount: "20",
+                    price: price,
+                    hidden: 0
+                }
+            ]));
+
+        }
+    }
+
+    getAllOrders = () => {
+        utils.log("AAQQUUII");
+        wss_auth.send(JSON.stringify([
+            CHAN_ID,
+            'os'
+        ]));
+    }
+
+};
+
+echoNestedArray = (array,parent="") => {
+    array.forEach(function(res, i){
+        if(Array.isArray(res)){
+            echoNestedArray(res,"["+i+"]");
+        }else{
+            utils.log(parent+"["+i+"]=>"+res);
+        }
+    });
 };
